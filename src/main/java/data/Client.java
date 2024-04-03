fae918c91988adb9a7bce8de8ad65bdc3c10e8c6
frxxzxmvnx <160493043+frxxzxmvnx@users.noreply.github.com>
2024-04-03 10:27:42 +0300

    Add files via upload

--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,4 +1,34 @@
 package data;
 
 public class Client {
+
+    private String name;
+    public String getName() {
+        return name;
+    }
+    private String surname;
+    public String getSurname() {
+        return surname;
+    }
+    private String isClient;
+
+    public String getIsClient() {
+        return isClient;
+    }
+
+    public void setIsClient(String isClient) {
+        this.isClient = isClient;
+    }
+
+    private String Card;
+
+    public String getCard() {
+        return Card;
+    }
+
+    public void setCard(String card) {
+        Card = card;
+    }
+
+
 }
